2ffc1486088cfc88ce6ef72777696126eed904f1
Leon <tahjib.leon399@gmail.com>
2023-11-20 20:42:13 +0600

    Navbar DONE

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,12 @@
-import { Link } from 'react-router-dom';
-import { FaFacebook, FaGithub } from "react-icons/fa";
+import { NavLink } from 'react-router-dom';
+import { FaFacebookF, FaGithub, FaBars, FaTwitter } from "react-icons/fa";
+import { RxCross1 } from "react-icons/rx";
+import { useState } from 'react';
 
 function Navbar() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
+
     // navItems
     const navItems = [
         { path: "/home", pageName: "Home" },
@@ -13,29 +18,46 @@ function Navbar() {
 
 
     return (
-        <header className="bg-black">
-            <nav className="p-4 flex gap-6">
+        <header className="bg-black text-white fixed top-0 left-0 right-0">
+            <nav className="p-4 max-w-7xl mx-auto flex justify-between items-center">
                 {/* logo  */}
-                <a href="/" className="text-xl font-bold text-white">Blog<span className="text-blue-500">Site</span></a>
+                <a href="/" className="text-3xl font-bold ">Blog<span className="text-blue-500">Site</span></a>
 
                 {/* Nav items for lg devices */}
-                <ul className='text-white md:flex gap-12 text-lg font-bold hidden'>
+                <ul className=' md:flex gap-12 text-lg font-medium hidden'>
                     {
                         navItems.map(({ path, pageName }) => <li key={pageName}>
-                            <Link to={path}>{pageName}</Link>
+                            <NavLink className={({ isActive }) =>
+                                isActive
+                                    ? "active"
+                                    : ""
+                            } to={path}>{pageName}</NavLink>
                         </li>)
                     }
                 </ul>
 
                 {/* menu items */}
-                <div className='text-white flex gap-3'>
-                    <a href="https://github.com/leon0113" className=' text-xl hover:text-blue-500'><FaFacebook /></a>
-                    <a href="https://github.com/leon0113" className=' text-xl hover:text-blue-500'><FaGithub /></a>
-                    <a href="https://github.com/leon0113" className=' text-xl hover:text-blue-500'><FaFacebook /></a>
+                <div className=' lg:flex gap-6 items-center hidden'>
+                    <a href="https://github.com/leon0113" className=' text-3xl hover:text-blue-500'><FaFacebookF /></a>
+                    <a href="https://github.com/leon0113" className=' text-3xl hover:text-blue-500'><FaGithub /></a>
+                    <a href="https://github.com/leon0113" className=' text-3xl hover:text-blue-500'><FaTwitter /></a>
                     <button className='bg-blue-500 py-2 px-6 font-medium rounded hover:bg-white hover:text-blue-500 transition-all duration-200 ease-in'>Log in</button>
                 </div>
-
+                {/* Mobile menu */}
+                <div className='md:hidden'>
+                    <button onClick={toggleMenu} className='cursor-pointer hover:text-blue-500'>{isMenuOpen ? <RxCross1 className='w-5 h-5' /> : <FaBars className='w-5 h-5' />}</button>
+                </div>
             </nav>
+            {/* mobile navigation bar */}
+            <div>
+                <ul className={`md:hidden gap-12 text-lg font-medium block space-y-4 px-4 py-6 mt-16 bg-gray-300 ${isMenuOpen ? "fixed top-0 left-0 w-full transition-all ease-out duration-0" : "hidden"}`} >
+                    {
+                        navItems.map(({ path, pageName }) => <li className='text-black' key={pageName}>
+                            <NavLink onClick={toggleMenu} to={path}>{pageName}</NavLink>
+                        </li>)
+                    }
+                </ul>
+            </div>
         </header>
     )
 }
